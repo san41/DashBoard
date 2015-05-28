9d2df23b2a1d2e25fdf33746fc45cdb1e4ba6362
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2015-05-28 22:12:43 +0200

    Fix bug when google clientID or clientSecret  are empty string

--- config/passport.js
+++ config/passport.js
@@ -117,7 +117,7 @@ passport.use(localStrategy);
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var GOOGLE_CLIENT_ID = config.google.auth.clientID;
 var GOOGLE_CLIENT_SECRET = config.google.auth.clientSecret;
-if(GOOGLE_CLIENT_ID != null && GOOGLE_CLIENT_SECRET != null){
+if(GOOGLE_CLIENT_ID != null && GOOGLE_CLIENT_ID.length > 1 && GOOGLE_CLIENT_SECRET != null && GOOGLE_CLIENT_SECRET.length > 1){
     MailBox.googleLoginEnable = true;
     passport.use('gmail-login', new GoogleStrategy({
         clientID: GOOGLE_CLIENT_ID,
