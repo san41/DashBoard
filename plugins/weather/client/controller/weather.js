f50feb97d3ce2d9ce0a5f6567c057e04ed798fc8
San37 <bleloup.pro@gmail.com>
2015-08-17 00:06:36 +0200

    Finish : Get weather

--- plugins/weather/client/controller/weather.js
+++ plugins/weather/client/controller/weather.js
@@ -1,123 +1,160 @@
 // TODO 
-//  Change metre/s en Km/h
+//  Vérifier la présence d'un ID lors de l'ajout dans la base de donnée des villes. si pas d'id tu retry et si le retry echoue tu n'accepte pas
+// Car on avait vu qu'une il avait pas charger direct
 //  Check the location to adapted translate (fr, en ,)
+// verifier le bug de la météo qui change 
 
-module.exports = function($scope, $http,socket, toaster){
+module.exports = function($scope,$http, socket, toaster){
+  $scope.weatherCount = null;
+  // view location div 
+  $scope.geolocation = false;
+  $scope.weatherList =null;
 
-  $scope.loadData = true;
+// Modal variable
   $scope.modal = null;
-
-$scope.weatherCount = 1;
-$scope.geolocation = "Location in progress";
-
-
-  // Var to modal 
   $scope.cityList = null;
 
 // Var longitude and latitude 
-var locationLat;
-var locationLng; 
-var lang = "en";
+    var locationLat = "";
+    var locationLng = ""; 
 
-// Get goelocation 
-
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-      $scope.geolocation = "Loading";
-      console.log("Get city Name ");
-      getGeolocationCityName(position.coords);
-      lat = position.coords.latitude; 
-      lng = position.coords.longitude;
-    });
-  }
-
-  // prov 
-
-  function update(){
-    console.log("update");
-      if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-      $scope.geolocation = "Loading";
-      console.log("Get city Name ");
-      getGeolocationCityName(position.coords);
-      lat = position.coords.latitude; 
-      lng = position.coords.longitude;
-    });
-  }
+// Check the lang. 
+  if(currentLanguage == ""){
+    var lang = "en";
+  }else{
+    var lang = currentLanguage;
   }
 
 
-  function getGeolocationCityName(position){
-    console.log("exc");
-    $scope.loadData = true;
-    var city; 
-    locationLat = position.latitude; 
-    locationLng = position.longitude;
-    // Load API Google Map with Latitude and Longitude
-    var GoogleMap = 'http://maps.google.com/maps/api/geocode/json?latlng='+locationLat+','+locationLng+'&sensor=false';
-    // Get Datas
-    $http.get(GoogleMap).success(function(response){
-      if(response.status == "OK"){
-        // Put City name in var
-        var city = response.results[1].formatted_address;
-        $scope.geolocation = city;
-        getWeatherLocation(locationLat, locationLng);
-        getWeatherPrediction(locationLat, locationLng);
-        return 
+setInterval(update, 50000);
+
+// update count weather to delete alert message : Add city. 
+  updateCount();
+  function updateCount(){
+    socket.emit('weather/count', function(err, count){
+      if(count >= 0){
+        $scope.weatherCount = count;
       }
+      return;
     });
   }
 
-
-  // Get live weather 
-
-  function getWeatherLocation(lat, lng){
-    var weatherLive = [];
-
-    // Load open weather map API to load live weather
-    var OpenWeahterMap = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8&lang='+lang+'';
-    console.log(OpenWeahterMap);
-    // Load prediction weather 
-    var OpenWeahterMapPrediction = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8&lang='+lang+'';
-    $http.get(OpenWeahterMap).success(function(responseWeatherToday){
-      //Load and convert temp data 
-      weatherLive['tempT'] = Math.round(responseWeatherToday.main['temp']);
-      weatherLive['temp_minT'] = Math.round(responseWeatherToday.main['temp_min']);
-      weatherLive['temp_maxT'] = Math.round(responseWeatherToday.main['temp_max']);
-      weatherLive['humidityT'] = Math.round(responseWeatherToday.main['humidity']);
-      weatherLive['pressureT'] = Math.round(responseWeatherToday.main['pressure']); 
-      weatherLive['descriptionT'] = responseWeatherToday.weather[0].description; 
-      weatherLive['icon'] = responseWeatherToday.weather[0].icon;
-      weatherLive['speedWindT'] = Math.round(responseWeatherToday.wind['speed']*3.6);  
-      $scope.weatherGeolocation = weatherLive;
-      console.log($scope.weatherGeolocation);
-      console.log("--------");
-      console.log(weatherLive); 
-      $scope.loadData = false;
-    });
+// Get current location and call function to get ciy name
+  getCurrentLocation();
+  function getCurrentLocation() {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+        locationLat = position.coords.latitude; 
+        locationLng = position.coords.longitude;
+        if(locationLat != "" && locationLng !=""){
+            // S'il n'y a pas de data dans la base mais la localisation est présente, supression du message d'alerte
+            $scope.weatherCount = 0;
+            $scope.geolocation = true;
+            getWeatherLocation(locationLat, locationLng);
+        }
+      });
+    }else{
+       console.log("Location error");
+    }
   }
 
+  function update(){     
+    if(locationLat != "" && locationLng !=""){
+      // S'il n'y a pas de data dans la base mais la localisation est présente, supression du message d'alerte
 
-  function getWeatherPrediction(lat, lng){
-    var WeatherPrediction = [];
-    var responseList = [];
-    // Load open weather map API to load prediction weather
-    var OpenWeahterMap = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8';
-      console.log(OpenWeahterMap);
-    $http.get(OpenWeahterMap).success(function(responseWeatherPrediction){
-      responseList = responseWeatherPrediction.list;
-      console.log(responseList);
-      for(var i = 0;  i < responseList.length   ; i++){
-        responseList[i].pressure = Math.round(responseList[i].pressure);
-        responseList[i].temp['day'] = Math.round(responseList[i].temp['day']);
-        responseList[i].temp['min'] = Math.round(responseList[i].temp['min']);
-        responseList[i].temp['max'] = Math.round(responseList[i].temp['max']);
-        responseList[i].speed = Math.round(responseList[i].speed *3.6);
+      $scope.geolocation = true;
+      getWeatherLocation(locationLat, locationLng);
+    }
+  }
+
+   // Get geolocation weather
+  function getWeatherLocation(lat, lng){      
+      var OpenWeatherMap = {live : 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8&lang='+lang, later: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8&lang='+lang };
+      var weather = { live:'', later:''}
+      $http.get(OpenWeatherMap.live).success(function(responseWeather){
+        if(responseWeather.cod == 200){
+            responseWeather.weather[0].id = responseWeather.weather[0].id + "-" +responseWeather.weather[0].icon.substr(2,2);
+            responseWeather.main['temp'] = Math.round(responseWeather.main['temp']);
+            responseWeather.main['temp_min'] = Math.round(responseWeather.main['temp_min']);
+            responseWeather.main['temp_max'] = Math.round(responseWeather.main['temp_max']);
+            responseWeather.main['humidity'] = Math.round(responseWeather.main['humidity']);
+            responseWeather.main['pressure'] = Math.round(responseWeather.main['pressure']); 
+            responseWeather.wind['speed'] = Math.round(responseWeather.wind['speed']*3.6);  
+            weather.live = responseWeather;   
+        }else{
+            toaster.pop('error','Erreur',"Impossible de charger la méteo du jour géolocalisé");
+        }
+      });
+      $http.get(OpenWeatherMap.later).success(function(responseWeather){
+        if(responseWeather.cod == 200){
+            var responseList = responseWeather.list;
+            for(var i = 0;  i < responseList.length   ; i++){
+                responseList[i].weather[0].id = responseList[i].weather[0].id + "-" +responseList[i].weather[0].icon.substr(2,2);
+                responseList[i].pressure = Math.round(responseList[i].pressure);
+                responseList[i].temp['day'] = Math.round(responseList[i].temp['day']);
+                responseList[i].temp['min'] = Math.round(responseList[i].temp['min']);
+                responseList[i].temp['max'] = Math.round(responseList[i].temp['max']);
+                responseList[i].speed = Math.round(responseList[i].speed *3.6);
+            }
+            weather.later = responseList;
+            $scope.weatherGeolocation = weather ;
+        }else{
+            toaster.pop('error','Erreur',"Impossible de charger la méteo géolocalisé");
+        }
+      });
+  }
+
+  // Get weather with location in database  
+
+  function getWeather(){
+    var weather = {};
+    var incre = 0;
+    var incre1 = 0;
+    socket.emit('weather/getList', function(data){
+      for(i = 0; i < data.length; i++){
+        var OpenWeatherMap = {live : 'http://api.openweathermap.org/data/2.5/weather?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang, later: 'http://api.openweathermap.org/data/2.5/forecast/daily?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang };
+        $http.get(OpenWeatherMap.live).success(function(responseWeather){
+          if(responseWeather.cod  == 200){
+            responseWeather.weather[0].id = responseWeather.weather[0].id + "-" +responseWeather.weather[0].icon.substr(2,2);
+            responseWeather.wind['speed'] = Math.round(responseWeather.wind['speed'] *3.6);
+            responseWeather.main.pressure = Math.round(responseWeather.main.pressure);
+            responseWeather.main.temp = Math.round(responseWeather.main.temp);
+            responseWeather.main.temp_min = Math.round(responseWeather.main.temp_min);
+            responseWeather.main.temp_max = Math.round(responseWeather.main.temp_max);
+            data[incre].weatherLive = responseWeather;
+          }  
+          incre++;       
+        });
       }
-      $scope.weatherGeolocationT = responseList;
+      for(i = 0; i < data.length; i++){
+        var OpenWeatherMap = {live : 'http://api.openweathermap.org/data/2.5/weather?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang, later: 'http://api.openweathermap.org/data/2.5/forecast/daily?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang };
+        $http.get(OpenWeatherMap.later).success(function(responseList){
+          if(responseList.cod  == 200){
+            for(var i = 0;  i < responseList.length   ; i++){
+              responseList[i].weather[0].id = responseList[i].weather[0].id + "-" +responseList[i].weather[0].icon.substr(2,2);
+              responseList[i].pressure = Math.round(responseList[i].pressure);
+              responseList[i].temp['day'] = Math.round(responseList[i].temp['day']);
+              responseList[i].temp['min'] = Math.round(responseList[i].temp['min']);
+              responseList[i].temp['max'] = Math.round(responseList[i].temp['max']);
+              responseList[i].speed = Math.round(responseList[i].speed *3.6);
+            }
+                        console.log(responseList)
+            data[incre1].weatherLater = responseList; 
+          }  
+          incre1++;
+        });
+      }
+      $scope.weatherList = data;
+      console.log($scope.weatherList)
     });
   }
 
+getWeather();
+  setInterval(getWeather, 50000);
+
+//####################################################################
+//  Function to add city with the form modal
+//####################################################################
   $scope.addWeatherLocation = function(){
     var newLocation = {
       editable : true,
@@ -125,93 +162,43 @@ var lang = "en";
     $scope.modal = newLocation;
   }
 
-setInterval(update, 500000);
-//  Auto update
-
-// Passer a select2
-
 // Check and send city data with OpenWeatherMap API. (Name, Country, id, lat, lng)
 // When there are ten result returned by the API, the user views a table with the data to add a city.
   $scope.cityName = function(){
     var cityName = $scope.modal.cityName;
-    var OpenWeahterMapCityID = 'http://api.openweathermap.org/data/2.5/find?q='+cityName+'&type=like&sort=population&cnt=30&lang=en'
+    var OpenWeatherMapCityID = 'http://api.openweathermap.org/data/2.5/find?q='+cityName+'&type=like&sort=population&cnt=30&lang=en'
     // To request API, The city name field must be minimum 3 characters.
     if(cityName.length > 2){
-    // Get city List to send
-      $http.get(OpenWeahterMapCityID).success(function(cityList){
+      $http.get(OpenWeatherMapCityID).success(function(cityList){
         if(cityList.cod == 200){
-          // Show a maximum of five cities
+          // Show ten cities
           if(cityList.count <= 10){
             $scope.cityList = cityList.list;
-            console.log($scope.cityList)
           }else{
             $scope.cityList = null;
           }
         }else{
-          toaster.pop('error','Error',"The database is not avaible, OWM is down ?");
+          toaster.pop('error','Error',"API does not sem available, OpenWeatherMap it would be off or call limit ?");
         }
       });
-    // reset tab with the reset input
     }else{
       $scope.cityList = null;
     }
   }
 
+// Function to add city id and name in the database :  Check
+  $scope.addCity = function(newCityData){
+    newCityData.position =  $scope.weatherCount +1;
+    socket.emit('weather/addCity', newCityData, function(err, res){
+      if(err){
+        toaster.pop('error', 'Error', err);
+        return;
+      }
+      toaster.pop('success','Success', 'Location has been added');
+      updateCount();
+    });
+  }
 
+  setInterval(update, 500000);
 
-  // link to search city data http://api.openweathermap.org/data/2.5/find?callback=?&q=yvoy&type=like&sort=population&cnt=30&lang=en
-
-//   $scope.modalNewFeed = function (){
-//     var newFeed = $scope.modal;
-//     // Check all information : TODO 
-//     // Change et evol information
-//   if(newFeed.name ==  null || newFeed.name == "" ){
-//     toaster.pop('error', 'Error', 'Title is empty !');
-//     return;
-//   }
-//   if(newFeed.link ==  null || newFeed.link == "" ){
-//     toaster.pop('error', 'Error', 'Link is empty or not valid !');
-//     return;
-//   }
-//   if(newFeed.category ==  null || newFeed.category == "" ){
-//     // todo add list
-//     toaster.pop('error', 'Error', 'Category is empty or not valid !');
-//     return;   
-//   }
-
-//     // Check
-//     socket.emit('feed/addfeed',newFeed, function(err, res){
-//       if(err){
-//         toaster.pop('error', 'Error', err);
-//         return;
-//       }
-//       toaster.pop('success', 'Success', 'RSS feed has been added');
-//       $scope.modal = null;
-//       $scope.rssfeedCount = 1;
-//       updateData();
-//     });
-//   }
-
-//   socket.emit('feed/list', function(err, list){
-//     if(list > 0){
-//       $scope.rssfeedCount = list;
-//       updateData();
-//     }else{ 
-//       $scope.rssfeedCount = 0;
-//     }
-//   });
-
-//   function updateData(){
-//     socket.emit('feed/read', function(result){
-//       $scope.rssload = true;
-//       $scope.rssfeed = [];
-//     });
-//   }
-
-//   setInterval(updateData, 100000);
-
-//   socket.on('get/feeds',function(data, callback){
-//     $scope.rssfeed = $scope.rssfeed.concat(data);
-//     $scope.rssload = false;
-//   });
 }
