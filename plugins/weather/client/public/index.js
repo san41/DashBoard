67888d6d778eb6cf2187a9b4f8d66cb31165b1f7
San37 <bleloup.pro@gmail.com>
2015-08-17 00:07:00 +0200

    Gulp

--- plugins/weather/client/public/index.js
+++ plugins/weather/client/public/index.js
@@ -1,82 +1,216 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function($scope, socket, toaster){
+// TODO 
+//  Vérifier la présence d'un ID lors de l'ajout dans la base de donnée des villes. si pas d'id tu retry et si le retry echoue tu n'accepte pas
+// Car on avait vu qu'une il avait pas charger direct
+//  Check the location to adapted translate (fr, en ,)
+// verifier le bug de la météo qui change 
 
-  $scope.rssfeed = null;
+module.exports = function($scope,$http, socket, toaster){
+  $scope.weatherCount = null;
+  // view location div 
+  $scope.geolocation = false;
+  $scope.weatherList =null;
+
+// Modal variable
   $scope.modal = null;
-  $scope.rssload = true;
+  $scope.cityList = null;
 
-// Start form to added the feed link
-  $scope.addRSSFeed = function(){
-    var newFeed = {
-      editable : true, 
-      categorysList : ["World", "Actuality", "Politics", "Business", "Techology","Science","Health","Sports","Arts","Style","Food","Traval","People","Work","Development" ],
+// Var longitude and latitude 
+    var locationLat = "";
+    var locationLng = ""; 
+
+// Check the lang. 
+  if(currentLanguage == ""){
+    var lang = "en";
+  }else{
+    var lang = currentLanguage;
+  }
+
+
+setInterval(update, 5000);
+
+// update count weather to delete alert message : Add city. 
+  updateCount();
+  function updateCount(){
+    socket.emit('weather/count', function(err, count){
+      if(count > 0){
+        $scope.weatherCount = count;
+      }
+      return;
+    });
+  }
+
+// Get current location and call function to get ciy name
+  getCurrentLocation();
+  function getCurrentLocation() {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+        locationLat = position.coords.latitude; 
+        locationLng = position.coords.longitude;
+        if(locationLat != "" && locationLng !=""){
+            // S'il n'y a pas de data dans la base mais la localisation est présente, supression du message d'alerte
+            $scope.weatherCount = 0;
+            $scope.geolocation = true;
+            getWeatherLocation(locationLat, locationLng);
+        }
+      });
+    }else{
+       console.log("Location error");
     }
-    $scope.modal = newFeed;
   }
 
-  $scope.modalNewFeed = function (){
-    var newFeed = $scope.modal;
-    // Check all information : TODO 
-    // Change et evol information
-  if(newFeed.name ==  null || newFeed.name == "" ){
-    toaster.pop('error', 'Error', 'Title is empty !');
-    return;
-  }
-  if(newFeed.link ==  null || newFeed.link == "" ){
-    toaster.pop('error', 'Error', 'Link is empty or not valid !');
-    return;
-  }
-  if(newFeed.category ==  null || newFeed.category == "" ){
-    // todo add list
-    toaster.pop('error', 'Error', 'Category is empty or not valid !');
-    return;   
+  function update(){     
+    if(locationLat != "" && locationLng !=""){
+      // S'il n'y a pas de data dans la base mais la localisation est présente, supression du message d'alerte
+      $scope.weatherCount = 0;
+      $scope.geolocation = true;
+      getWeatherLocation(locationLat, locationLng);
+    }
   }
 
-    // Check
-    socket.emit('feed/addfeed',newFeed, function(err, res){
+   // Get geolocation weather
+  function getWeatherLocation(lat, lng){      
+      var OpenWeatherMap = {live : 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8&lang='+lang, later: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+lat+'&lon='+lng+'&mode=json&units=metric&cnt=8&lang='+lang };
+      var weather = { live:'', later:''}
+      $http.get(OpenWeatherMap.live).success(function(responseWeather){
+        if(responseWeather.cod == 200){
+            responseWeather.weather[0].id = responseWeather.weather[0].id + "-" +responseWeather.weather[0].icon.substr(2,2);
+            responseWeather.main['temp'] = Math.round(responseWeather.main['temp']);
+            responseWeather.main['temp_min'] = Math.round(responseWeather.main['temp_min']);
+            responseWeather.main['temp_max'] = Math.round(responseWeather.main['temp_max']);
+            responseWeather.main['humidity'] = Math.round(responseWeather.main['humidity']);
+            responseWeather.main['pressure'] = Math.round(responseWeather.main['pressure']); 
+            responseWeather.wind['speed'] = Math.round(responseWeather.wind['speed']*3.6);  
+            weather.live = responseWeather;   
+        }else{
+            toaster.pop('error','Erreur',"Impossible de charger la méteo du jour géolocalisé");
+        }
+      });
+      $http.get(OpenWeatherMap.later).success(function(responseWeather){
+        if(responseWeather.cod == 200){
+            var responseList = responseWeather.list;
+            for(var i = 0;  i < responseList.length   ; i++){
+                responseList[i].weather[0].id = responseList[i].weather[0].id + "-" +responseList[i].weather[0].icon.substr(2,2);
+                responseList[i].pressure = Math.round(responseList[i].pressure);
+                responseList[i].temp['day'] = Math.round(responseList[i].temp['day']);
+                responseList[i].temp['min'] = Math.round(responseList[i].temp['min']);
+                responseList[i].temp['max'] = Math.round(responseList[i].temp['max']);
+                responseList[i].speed = Math.round(responseList[i].speed *3.6);
+            }
+            weather.later = responseList;
+            $scope.weatherGeolocation = weather ;
+        }else{
+            toaster.pop('error','Erreur',"Impossible de charger la méteo géolocalisé");
+        }
+      });
+  }
+
+  // Get weather with location in database  
+
+  function getWeather(){
+    var weather = {};
+    var incre = 0;
+    var incre1 = 0;
+    socket.emit('weather/getList', function(data){
+      for(i = 0; i < data.length; i++){
+        var OpenWeatherMap = {live : 'http://api.openweathermap.org/data/2.5/weather?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang, later: 'http://api.openweathermap.org/data/2.5/forecast/daily?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang };
+        $http.get(OpenWeatherMap.live).success(function(responseWeather){
+          if(responseWeather.cod  == 200){
+            responseWeather.weather[0].id = responseWeather.weather[0].id + "-" +responseWeather.weather[0].icon.substr(2,2);
+            responseWeather.wind['speed'] = Math.round(responseWeather.wind['speed'] *3.6);
+            responseWeather.main.pressure = Math.round(responseWeather.main.pressure);
+            responseWeather.main.temp = Math.round(responseWeather.main.temp);
+            responseWeather.main.temp_min = Math.round(responseWeather.main.temp_min);
+            responseWeather.main.temp_max = Math.round(responseWeather.main.temp_max);
+            data[incre].weatherLive = responseWeather;
+          }  
+          incre++;       
+        });
+      }
+      for(i = 0; i < data.length; i++){
+        var OpenWeatherMap = {live : 'http://api.openweathermap.org/data/2.5/weather?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang, later: 'http://api.openweathermap.org/data/2.5/forecast/daily?id='+data[i].cityid+'&mode=json&units=metric&cnt=8&lang='+lang };
+        $http.get(OpenWeatherMap.later).success(function(responseList){
+          if(responseList.cod  == 200){
+            for(var i = 0;  i < responseList.length   ; i++){
+              responseList[i].weather[0].id = responseList[i].weather[0].id + "-" +responseList[i].weather[0].icon.substr(2,2);
+              responseList[i].pressure = Math.round(responseList[i].pressure);
+              responseList[i].temp['day'] = Math.round(responseList[i].temp['day']);
+              responseList[i].temp['min'] = Math.round(responseList[i].temp['min']);
+              responseList[i].temp['max'] = Math.round(responseList[i].temp['max']);
+              responseList[i].speed = Math.round(responseList[i].speed *3.6);
+            }
+            data[incre1].weatherLater = responseList; 
+          }  
+          incre1++;
+        });
+      }
+      $scope.weatherList = data;
+      console.log($scope.weatherList)
+    });
+  }
+
+
+  setInterval(getWeather, 5000);
+
+//####################################################################
+//  Function to add city with the form modal
+//####################################################################
+  $scope.addWeatherLocation = function(){
+    var newLocation = {
+      editable : true,
+    }
+    $scope.modal = newLocation;
+  }
+
+// Check and send city data with OpenWeatherMap API. (Name, Country, id, lat, lng)
+// When there are ten result returned by the API, the user views a table with the data to add a city.
+  $scope.cityName = function(){
+    var cityName = $scope.modal.cityName;
+    var OpenWeatherMapCityID = 'http://api.openweathermap.org/data/2.5/find?q='+cityName+'&type=like&sort=population&cnt=30&lang=en'
+    // To request API, The city name field must be minimum 3 characters.
+    if(cityName.length > 2){
+      $http.get(OpenWeatherMapCityID).success(function(cityList){
+        if(cityList.cod == 200){
+          // Show ten cities
+          if(cityList.count <= 10){
+            $scope.cityList = cityList.list;
+          }else{
+            $scope.cityList = null;
+          }
+        }else{
+          toaster.pop('error','Error',"API does not sem available, OpenWeatherMap it would be off or call limit ?");
+        }
+      });
+    }else{
+      $scope.cityList = null;
+    }
+  }
+
+// Function to add city id and name in the database :  Check
+  $scope.addCity = function(newCityData){
+    newCityData.position =  $scope.weatherCount +1;
+    socket.emit('weather/addCity', newCityData, function(err, res){
       if(err){
         toaster.pop('error', 'Error', err);
         return;
       }
-      toaster.pop('success', 'Success', 'RSS feed has been added');
-      $scope.modal = null;
-      $scope.rssfeedCount = 1;
-      updateData();
+      toaster.pop('success','Success', 'Location has been added');
+      updateCount();
     });
   }
 
-  socket.emit('feed/list', function(err, list){
-    if(list > 0){
-      $scope.rssfeedCount = list;
-      updateData();
-    }else{ 
-      $scope.rssfeedCount = 0;
-    }
-  });
+  setInterval(update, 500000);
 
-  function updateData(){
-    socket.emit('feed/read', function(result){
-      $scope.rssload = true;
-      $scope.rssfeed = [];
-    });
-  }
-
-  setInterval(updateData, 100000);
-
-  socket.on('get/feeds',function(data, callback){
-    $scope.rssfeed = $scope.rssfeed.concat(data);
-    $scope.rssload = false;
-  });
 }
 },{}],2:[function(require,module,exports){
-var plugin = getPlugin('rssreader');
+var plugin = getPlugin('weather');
 
-plugin.registerController('RssReaderController', require('./controller/rssreader.js'));
+plugin.registerController('WeatherController', require('./controller/weather.js'));
 
-plugin.registerRoute('/rssreader', {
-  controller: 'RssReaderController',
+plugin.registerRoute('/weather', {
+  controller: 'WeatherController',
   templateUrl: 'index.html'
 });
 
-plugin.registerMenuItem('RssReader', '/rssreader', 'fa-rss-square');
-},{"./controller/rssreader.js":1}]},{},[2])
+plugin.registerMenuItem('Weather', '/weather', 'fa-sun-o');
+},{"./controller/weather.js":1}]},{},[2])
--- plugins/weather/client/public/settings.js
+++ plugins/weather/client/public/settings.js
@@ -1,54 +1,50 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function($scope, socket, $location, $routeParams, toaster){
+  $scope.weatherList = [];
 
-  $scope.rssfeeds = [];
-  $scope.editmodal = null;
-
-  socket.emit('rssreader/list', function(err, list){
-    if(err) return;
-    $scope.rssfeeds = list; 
+  // Request weather list added in database by user 
+  socket.emit('weather/getList', function(list){
+    if(list.length < 1){
+      toaster.pop('error', 'Error', 'No city added to the database.');
+    }
+    $scope.weatherList  =  list;
   });
 
-  $scope.delete = function(feed){
-    if(!confirm('Are you sure to delete the rss feed ' + feed.name +' ?')){
+  // Delete weather location
+
+  $scope.delete =  function(weather){
+    if(!confirm('Are you sure to delete the '+ weather.name+' city ?')){
       return;
     }
-    socket.emit('rssreader/delete', feed, function(err){
+    socket.emit('weather/settings/delete', weather, function(err){
       if(err){
-        toaster.pop('error', 'Error', 'Cannot delete this rss feed'); 
+        toaster.pop('error','Error','Cannot delete this weather');
         return;
       }
-
-      var index = -1;
-      for(var i in $scope.rssfeeds){
-        var scopeRssfeeds = $scope.rssfeeds[i];
-        if(scopeRssfeeds._id == feed._id){
+      var index = -1; 
+      for(var i in $scope.weatherList){
+        var scopeweatherList = $scope.weatherList[i];
+        if(scopeweatherList._id == weather._id){
           index = i;
         }
       }
       if(index > -1){
-        $scope.rssfeeds.splice(index, 1);
+        $scope.weatherList.splice(index,1);
       }
     });
   }
-
-  $scope.edit = function(feed){
-    console.log(feed);
-    $scope.editmodal = true;
-    toaster.pop('error', 'Error', 'Edit is not avaible');
-  }
 }
 },{}],2:[function(require,module,exports){
-var settings = getPlugin("rssreader").settings;
+var settings = getPlugin("weather").settings;
 
 
-settings.registerController("FeedListController", require('./controller/feedList'));
+settings.registerController("WeatherSettingController", require('./controller/weather'));
 
 
-settings.registerRoute('/rssreader',{
-  controller :  'FeedListController',
+settings.registerRoute('/weather',{
+  controller :  'WeatherSettingController',
   templateUrl :  'index.html'
 });
 
-settings.registerSettingsItem('RssReader', '/rssreader', 'fa-rss-square');
-},{"./controller/feedList":1}]},{},[2])
+settings.registerSettingsItem('Weather', '/weather', 'fa-sun-o');
+},{"./controller/weather":1}]},{},[2])
